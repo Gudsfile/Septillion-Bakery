34bf9fa909bc248d23978ac4c44b25b133548bf4
Gudsfile <theo.chepas@gmail.com>
2018-10-11 10:32:33 +0200

    rewrite contact.php

--- septillion-web/connexion.php
+++ septillion-web/connexion.php
@@ -4,7 +4,7 @@
 {
     public static function connexion() {
         try {
-    return new PDO("mysql:host=localhost;dbname=septillion","root","123456");
+    return new PDO("mysql:host=localhost;dbname=septillion","root","root");
         }
         catch(PDOException $e) {
             die('<h3>Erreur!</h3>');
