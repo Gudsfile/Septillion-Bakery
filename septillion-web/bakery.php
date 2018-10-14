9e60607ec9348acfedbf332bbc0166e8adc8a26e
Théophile <theo.chepas@gmail.com>
2018-10-14 23:42:43 +0200

    add bakery fun

---
+++ septillion-web/bakery.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <link rel="stylesheet" href="css/bakery.css">
+</head>
+
+<body>
+  <div class="illustration">
+    <!--ground-->
+    <div class="ground"></div>
+    <!--lantern-->
+    <div class="lantern">
+      <!--lantern top part-->
+      <div class="lantern__top"></div>
+      <!--lantern middle part-->
+      <div class="lantern__middle"></div>
+      <!--lantern bottom part-->
+      <div class="lantern__bottom"></div>
+    </div>
+    <!--bakery-->
+    <div class="bakery">
+      <!--base -->
+      <div class="bakery__base"></div>
+      <!--walls -->
+      <div class="bakery__walls-basic">
+        <!-- front part walls-->
+        <div class="bakery__front-part">
+          <!--left window -->
+          <div class="bakery__window bakery__window--left">
+            <!--shelf 1-->
+            <div class="shelf shelf--with-border">
+              <div class="dessert-pack dessert-pack--top dessert-pack--bigger-dimension">
+                <!--dessert 4 -->
+                <div class="dessert dessert--4"></div>
+                <!--dessert 5 -->
+                <div class="dessert dessert--5"></div>
+              </div>
+            </div>
+            <!--shelf 2-->
+            <div class="shelf shelf--with-border">
+              <div class="dessert-pack dessert-pack--top">
+                <!--dessert 1 -->
+                <div class="dessert dessert--1"></div>
+                <!--dessert 2 -->
+                <div class="dessert dessert--2"></div>
+                <!--dessert 3 -->
+                <div class="dessert dessert--3"></div>
+              </div>
+            </div>
+            <!--shelf 3-->
+            <div class="shelf">
+              <div class="dessert-pack dessert-pack--bottom">
+                <div class="donut"></div>
+                <div class="donut"></div>
+                <div class="donut"></div>
+                <div class="donut"></div>
+                <div class="donut"></div>
+              </div>
+            </div>
+          </div>
+          <!-- door -->
+          <div class="bakery__door"></div>
+          <!--right window -->
+          <div class="bakery__window bakery__window--right">
+            <!-- cake -->
+            <div class="cake">
+              <!--cake top part-->
+              <div class="cake__part cake__part--top"></div>
+              <!--cake bottom part-->
+              <div class="cake__part cake__part--bottom"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--roof-->
+      <div class="bakery__roof-basic">
+        <!--chimney-->
+        <div class="bakery__chimney">
+          <!--smoke-->
+          <div class="smoke"></div>
+        </div>
+        <!-- roof: front part-->
+        <div class="bakery__roof-front-part">
+          <!-- top block -->
+          <div class="bakery__roof-top-block">
+            <!--small decorations block -->
+            <div class="bakery__roof-top-decorations"></div>
+            <!--main sign block-->
+            <div class="bakery__roof-main-sign">
+              <p>Bakery</p>
+            </div>
+          </div>
+          <!-- bottom block -->
+          <div class="bakery__roof-bottom-block"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script  src="js/index.js"></script>
+
+</body>
+
+</html>
+
+
+
+<!--
+Copyright (c) 2018 by fox_hover (https://codepen.io/fox_hover/pen/bLeGME)
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
