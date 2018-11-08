319da7a9493b760f8fb0de67a17ff558e2d353d0
Yann <y.legoff97@gmail.com>
2018-11-08 10:58:16 +0100

    Petite correction d'orthographe

--- septillion-web/back-office/add_category.php
+++ septillion-web/back-office/add_category.php
@@ -128,7 +128,7 @@
         <div class="alert bg-warning" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> Erreur BDD </div>
       <?php ; endif ?>
       <?php if ($erreur == '3'): ?>
-        <div class="alert bg-success" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> Produit ajouté !</div>
+        <div class="alert bg-success" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> Catégorie ajoutée !</div>
       <?php ; endif ?>
     </div>
   </div>
